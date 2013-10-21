a16d08772392293555fc581fea37302d5a98c2b1
Kohei Shingai <koheishingai@gmail.com>
2013-10-22 00:43:07 +0900

    Add japanese notation

---
+++ README .ja.markdown
@@ -0,0 +1,7 @@
+## It's a Thing!
+
+HTML5 ドラッグ&ドロップでプロフィール画像を変更(リサイズ/トリミング機能付)
+
+デモ: http://css-tricks.com/examples/DragAvatar/
+
+記事: http://css-tricks.com/html5-drag-and-drop-avatar-changer-with-resizing-and-cropping
---
+++ language.js
@@ -0,0 +1,26 @@
+//Detecting browser language preference and set suitable text
+(function(){
+
+  //Detecting browser language preference function
+  function getlang() {
+    try {
+      return (navigator.browserLanguage || navigator.language ||   navigator.userLanguage).substr(0,2)
+    }catch(e) {
+    return undefined;
+    }
+  }
+  
+  //If it is japanese
+  if(getlang() == "ja"){
+    $('body').css("font-family", "'メイリオ', 'ヒラギノ角ゴ Pro W3', 'MS PGothic', 'MS UI Gothic'");
+    $(".page-wrap h1").css('font-size', '25px').text("ドラッグ&ドロップでプロフィール画像をチェンジ");  
+    $(".page-wrap h2").css('font-size', '20px').text("ベティー ミラー");
+    $(".location").css('font-size', '14px').text("パロアルト, カリフォルニア州");
+  
+    $(".profile p").css('font-size', '13px').text("職業は教師だそうだ。学校から帰ると終日書斎に這入ったぎりほとんど出て来る事がない。家のものは大変な勉強家だと思っている。当人も勉強家であるかのごとく見せている。");
+    
+    $(".page-wrap h3").css('font-size','16px').text("ファイルを直接アップロードすることも出来ます。");
+  }
+
+
+})();
